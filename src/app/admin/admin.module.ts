5dc91e9302bb7eb3bcfefaaff1c61ea77a6ea617
viraj2252@gmail.com <viraj2252@gmail.com>
2017-07-08 15:08:35 +0800

    Completed admin module for blog posts and public view

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,12 +9,19 @@ import { LoginComponent }    from './login/login.component';
 import { SignUpComponent }    from './signUp/sign-up.component';
 
 import { UserService } from './adminShared/user.service';
+import { BlogAdminService } from './adminShared/blog-admin.service';
+
+import { BlogAdminComponent } from './blogAdmin/blog-admin.component';
+import { BlogAddComponent } from './blogAdd/blog-add.component';
+
+import { TruncatePipe } from './adminShared/trunc.pipe';
 
 const AdminRoutes: Routes = [
     {
         path: 'admin',
         component: AdminComponent,
         children: [
+            { path: 'blog-admin', component: BlogAdminComponent, canActivate: [UserService] },
             { path: 'login', component: LoginComponent },
             { path: 'signup', component: SignUpComponent },
             { path: '', component: AdminMenuComponent, canActivate: [UserService] }            
@@ -28,16 +35,21 @@ const AdminRoutes: Routes = [
         RouterModule.forChild(AdminRoutes)
     ],
     exports: [
-        RouterModule
+        RouterModule,
+        TruncatePipe
     ],
     declarations: [
         AdminComponent,
         AdminMenuComponent,
         LoginComponent,
-        SignUpComponent
+        SignUpComponent,
+        BlogAdminComponent,
+        BlogAddComponent,
+        TruncatePipe
     ],
     providers: [
         UserService,
+        BlogAdminService
     ]
 })
 export class AdminModule {}
